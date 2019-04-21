2797f117330eccc8bae37c97c7b2faf62c64afcc
Anton <scoutrul@mail.ru>
2019-04-21 21:38:25 +0300

    store creation

--- store/index.js
+++ store/index.js
@@ -1,29 +1,28 @@
-import Vue from 'vue'
 import Vuex from 'vuex'
 
 import actions from './actions'
 import mutations from './mutations'
 import getters from './getters'
 
-Vue.use(Vuex)
-
-const store = new Vuex.Store({
-  state: {
-    teams: {},
-    teamsCount: 0,
-    leagues: {},
-    leaguesCount: 0,
-    fixtures: {},
-    liveFixtures: {},
-    todayFixtures: {},
-    lineups: {},
-    events: {},
-    statistics: {},
-    leagueTables: {}
-  },
+const createStore = () => {
+  return new Vuex.Store({
+    state: () => ({
+      teams: {},
+      teamsCount: 0,
+      leagues: {},
+      leaguesCount: 0,
+      fixtures: {},
+      liveFixtures: {},
+      todayFixtures: {},
+      lineups: {},
+      events: {},
+      statistics: {},
+      leagueTables: {}
+    }),
   getters,
   mutations,
   actions
-})
+  })
+}
 
-export default store
+export default createStore
