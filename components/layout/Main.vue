fd73960dc01cee0c2ab3aeb9278bdf6721dc3268
Anton <scoutrul@mail.ru>
2019-04-29 22:12:49 +0300

    styles

--- components/layout/Main.vue
+++ components/layout/Main.vue
@@ -1,13 +1,3 @@
 <template>
   <v-layout column class="main"> <slot /> </v-layout>
 </template>
-
-<script>
-export default {}
-</script>
-<style>
-.main {
-  margin-top: 60px;
-  z-index: 0;
-}
-</style>
